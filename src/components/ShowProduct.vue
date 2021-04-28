549c4a07a5c016b1fc7d9c4d4fcdaacf4025518e
Thanasap-L <thanasap.lee@outlook.com>
2021-04-28 10:51:00 +0700

    for pull

--- src/components/ShowProduct.vue
+++ src/components/ShowProduct.vue
@@ -1,9 +1,22 @@
 <template>
-    
+    <span class="go-back">
+        <button @click="goBack">Back</button>
+    </span>
 </template>
 <script>
 export default {
     name: "ShowProduct",
-
+    methods:(){
+        goBack(){
+            return this.$router.go(-1);
+        }
+    }
 }
 </script>
+
+<style scoped>
+.go-back{
+    display: flex;
+    cursor: pointer;
+}
+</style>
