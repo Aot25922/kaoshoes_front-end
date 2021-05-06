983ce12761f08a2389b4248f595f862c51651142
nithach2321 <bom-_2321@outlook.co.th>
2021-05-06 18:27:43 +0700

    Change dbUrl

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,8 @@ export default {
 } ,
   data(){
     return {
-      dbUrl : "http://spring:8080"
-      // dbUrl : "http://kaoshoes.ddns.net:8080"
+      dbUrl : "http://spring:8443"
+      // dbUrl : "http://kaoshoes.ddns.net:8443"
     }
   },
   methods : {
